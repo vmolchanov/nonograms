de781d456a14ddf028a240ab87d75e990cae7b62
Vladislav Molchanov <Molchanov96@yandex.ru>
2021-01-09 16:34:49 +0300

    Добавил вотчер на изменение картинок

--- gulp-scripts/tasks/server-dev.js
+++ gulp-scripts/tasks/server-dev.js
@@ -3,6 +3,7 @@ const {server, developmentConfig: config, reload} = require('../components/serve
 const html = require('./html');
 const styles = require('./styles');
 const scripts = require('./scripts');
+const images = require('./images');
 
 module.exports = () => {
     server.init(config);
@@ -10,4 +11,5 @@ module.exports = () => {
     gulp.watch('./src/*.html').on('change', gulp.series(html, reload));
     gulp.watch('./src/scss/**/*.scss').on('change', gulp.series(styles, reload));
     gulp.watch('./src/js/**/*.js').on('change', gulp.series(scripts, reload));
+    gulp.watch('./src/img/**/*.*').on('change', gulp.series(images, reload));
 };
